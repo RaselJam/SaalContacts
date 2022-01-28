379b786ce6008cf87e1f1dd5c6892ed2a7098f00
RaselJam <Rasel.jam.h@gmail.com>
2022-01-28 14:32:01 +0100

    removed unused imports + FIXED semantic HTML

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -28,13 +28,13 @@ const HomePage: React.FC = (props) => {
     <div className="flex flex-col w-screen h-screen overflow-auto text-gray-700 bg-gray-800 ">
       <div className="flex items-center flex-shrink-0 w-full h-16 px-10 bg-white bg-opacity-25">
       <button className="flex items-center justify-center w-8 h-8 ml-6 mr-auto overflow-hidden rounded-full cursor-pointer">
-         <a href="https://github.com/RaselJam" target="_blank"> <img
+         <a href="https://github.com/RaselJam" target="_blank" rel="noreferrer"> <img
             src="https://avatars.githubusercontent.com/u/73784313?s=400&u=053e3e3f4ea4f8900e9c9ba57c3f279ae095a2a6&v=4"
             alt=""
           ></img></a>
         </button>
-        <a href="https://www.saal-digital.es/" target="_blank">
-          <img src ="https://i.pinimg.com/280x280_RS/92/5f/a0/925fa0f5ed17086d847cc833ca82a562.jpg" className="w-11 h-11  hover:shadow   " />
+        <a href="https://www.saal-digital.es/" target="_blank" rel="noreferrer">
+          <img src ="https://i.pinimg.com/280x280_RS/92/5f/a0/925fa0f5ed17086d847cc833ca82a562.jpg" alt="saal Logo" className="w-11 h-11  hover:shadow   " />
         </a>
         <input
           className="flex items-center h-10 px-4 ml-10 text-sm w-1/3  bg-gray-200 rounded-full focus:outline-none focus:ring"
--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import {
   BsFillPenFill,
   BsFillTrashFill,
-  BsFillCalendar3WeekFill,
   BsMailbox2,
   BsTelephoneFill,
   BsFilePerson,
